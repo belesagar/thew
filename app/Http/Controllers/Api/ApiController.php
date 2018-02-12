18381cdc7800628c0f16f3c8a4d1c96a25a4ef85
sagar_bele <bele.sagar10@gmail.com>
2018-02-12 19:28:12 +0530

    This new file changes

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\franchiseModel;
+use Session;
+use Mail; 
+
+class ApiController extends Controller
+{
+
+    public function index() {
+		return view('dashboard');
+    }
+	
+	public function getdata() {
+		return view("admin.home");
+    }
+	
+	public function login() {
+		return view("admin.login");
+    }
+	
+	public function signup() {
+		return view("signup");
+    }
+	
+	public function checkSignup(Request $request) {
+		$request_data = $request->all();
+		return $request_data['name'];
+    }
+
+    public function checkUser(Request $request) {
+		dd($request);
+    }
+}
